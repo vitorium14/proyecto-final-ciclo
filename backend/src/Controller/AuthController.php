376170cee1d2a6b85c547e4334b469dedc24fbb7
Victor Gimenez <vitorium14@gmail.com>
2025-04-29 14:14:59 +0200

    Arreglado security, tema tokens

--- backend/src/Controller/AuthController.php
+++ backend/src/Controller/AuthController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Doctrine\ORM\EntityManagerInterface;
